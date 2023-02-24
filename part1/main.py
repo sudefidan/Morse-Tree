2fc34883cd6db0c10e7542bdc1f379bdc410f9a4
s2-fidan <sude2.fidan@live.uwe.ac.uk>
2023-02-24 15:49:14 +0000

    adding messaging and space encoding

--- part1/main.py
+++ part1/main.py
@@ -3,8 +3,24 @@ from morse import MorseTree
 
 if __name__ == "__main__":
     morse = MorseTree()
+
+    #task2
     e = morse.encode('us') 
     print('%s' % e)
     d = morse.decode(e)
     assert morse.encode('us') == '..- ...', "Should be ..-" 
     #assert morse.decode('..- ...') == 'us', "Should be ..-"
+
+    #messages
+    print('--------TEXT TO MORSE-------------')
+    text_message = input(('Enter text to encode ==> '))
+    encoded = morse.encode(text_message)
+    print('Encoded message: ' + encoded)
+
+    print('--------MORSE TO TEXT-------------')
+    morse_message = input(('Enter morse to decode ==> '))
+    decoded = morse.decode(morse_message)
+    print('Decoded message: ' + decoded)
+
+    #printing morse tree
+    morse.print_tree(morse.root)
--- part1/morseunit.py
+++ part1/morseunit.py
@@ -2,58 +2,63 @@ import unittest
 from morse import MorseTree
 
 class TestMorse(unittest.TestCase):
-    def setUp(self):
-        self.morse = MorseTree()
-
     #task3
     def test_encode(self):
-        self.assertEqual( self.morse.encode('us'), '..- ...') #true
-        self.assertEqual( self.morse.encode('sude'), '... ..- -.. .') #true
-        self.assertEqual(self.morse.encode('iot'), '.. --- -') #true
-        self.assertNotEqual(self.morse.encode('fidan'), '..-. .. ... .- -.') #false
-        self.assertNotEqual( self.morse.encode('benedict'), '-... . -. . -..') #false
+        self.assertEqual( tree.encode('us'), '..- ...') #true
+        self.assertEqual( tree.encode('Sude Fidan'), '... ..- -.. . / ..-. .. -.. .- -.') #true
+        self.assertEqual(tree.encode('2023'), '..--- ----- ..--- ...--') #true
+        self.assertEqual(tree.encode('2023 + 2017'), '..--- ----- ..--- ...-- / .-.-. / ..--- ----- .---- --...') #true
+        self.assertEqual(tree.encode('FIDAN'), '..-. .. -.. .- -.') #true
+        self.assertEqual(tree.encode('WE LOVE PC !'), '.-- . / .-.. --- ...- . / .--. -.-. / -.-.--') #true
+        self.assertNotEqual( tree.encode('benedict'), '-... . -. . -..') #false - benedict: -... . -. . -.. .. -.-. -
+        self.assertNotEqual( tree.encode('internet of the things'), '.. -. - . .-. -. . - / --- ..-. / - .... . / - .... .. -. --.') #false - internet of the things:.. -. - . .-. -. . - / --- ..-. / - .... . / - .... .. -. --. ...
         
     def test_decode(self):
-        self.assertEqual( self.morse.decode('...- ... -.-. --- -.. .'), 'VSCODE') #true
-        self.assertEqual( self.morse.decode('... ..-.'), 'SF') #true
-        self.assertEqual( self.morse.decode('- .... .'), 'THE') #true
-        self.assertNotEqual( self.morse.decode('.. -. - . .-. -.'), 'INTERNET') #false
-        self.assertNotEqual( self.morse.decode('- .... .. -. --. .'), 'THINGS') #false
+        self.assertEqual( tree.decode('...- ... -.-. --- -.. .'), 'VSCODE') #true
+        self.assertEqual( tree.decode('... ..-.'), 'SF') #true
+        self.assertEqual( tree.decode('- .... . / . -. -..'), 'THE END') #true
+        self.assertEqual( tree.decode('..--- ----- ..--- ...-- / -....- / ..--- ----- ..--- ....-'), '2023 - 2024') #true
+        self.assertNotEqual( tree.decode('.. -. - . .-. -.'), 'INTERNET') #false - INTERNET: .. -. - . .-. -. . -
+        self.assertNotEqual( tree.decode('- .... .. -. --. .'), '2002') #false - 2002: ..--- ----- ----- ..---
         
     def test_is_empty(self):
-        self.assertFalse(self.morse.is_empty()) #true
+        self.assertFalse(tree.is_empty()) #true
 
     def test_is_not_empty(self):
-        self.assertTrue(self.morse.is_not_empty()) #true
+        self.assertTrue(tree.is_not_empty()) #true
 
     def test_find(self):
-        self.assertIn(self.morse.find('.-'), 'A') #true
+        self.assertIn(tree.find('.-'), tree.dict) #true
 
     def test_insert(self):
-        self.morse.insert('.--.-.','@') 
-        self.assertEqual(self.morse.find('.--.-.'), '@') #true
-        self.assertIn(self.morse.find('.--.-.'),self.morse.dict) #true
-        self.assertNotIn(self.morse.find('.........'),self.morse.dict) #true - doesn't exist in dictionary
+        tree.insert('.--.-.','@') 
+        self.assertEqual(tree.find('.--.-.'), '@') #true
+        self.assertIn(tree.find('.--.-.'),tree.dict) #true
+        self.assertNotIn(tree.find('.........'),tree.dict) #true - doesn't exist in dictionary
 
     #task4
     def test_symbols(self):
-        self.assertEqual(self.morse.find('.-.-.-'), '.') #true
-        self.assertEqual(self.morse.find('-.--.'), '(') #true
-        self.assertEqual(self.morse.find('.-.-.'), '+') #true
-        self.assertEqual(self.morse.find('..-.-'), '¿') #true
-        self.assertEqual(self.morse.find('--..--'), ',') #true
-        self.assertEqual(self.morse.find('-.--.-'), ')') #true
-        self.assertEqual(self.morse.find('-....-'), '-') #true
-        self.assertEqual(self.morse.find('--...-'), '¡') #true
-        self.assertEqual(self.morse.find('..--..'), '?') #true
-        self.assertEqual(self.morse.find('.-...'), '&') #true
-        self.assertEqual(self.morse.find('..--.-'), '_') #true 
-        self.assertEqual(self.morse.find('.----.'), '’') #true 
-        self.assertEqual(self.morse.find('---...'), ':') #true 
-        self.assertEqual(self.morse.find('.-..-.'), '”') #true 
-        self.assertEqual(self.morse.find('-.-.--'), '!') #true 
-        self.assertEqual(self.morse.find('-.-.-.'), ';') #true 
-        self.assertEqual(self.morse.find('...-..-'), '$') #true 
+        #TODO ADD SOME OTHER CHALLEGING TESTINGS
+        self.assertEqual(tree.find('.-.-.-'), '.') #true
+        self.assertEqual(tree.find('-.--.'), '(') #true
+        self.assertEqual(tree.find('.-.-.'), '+') #true
+        self.assertEqual(tree.find('..-.-'), '¿') #true
+        self.assertEqual(tree.find('--..--'), ',') #true
+        self.assertEqual(tree.find('-.--.-'), ')') #true
+        self.assertEqual(tree.find('-....-'), '-') #true
+        self.assertEqual(tree.find('--...-'), '¡') #true
+        self.assertEqual(tree.find('..--..'), '?') #true
+        self.assertEqual(tree.find('.-...'), '&') #true
+        self.assertEqual(tree.find('..--.-'), '_') #true 
+        self.assertEqual(tree.find('.----.'), '’') #true 
+        self.assertEqual(tree.find('---...'), ':') #true 
+        self.assertEqual(tree.find('.-..-.'), '”') #true 
+        self.assertEqual(tree.find('-.-.--'), '!') #true 
+        self.assertEqual(tree.find('-.-.-.'), ';') #true
+        self.assertIn(tree.decode('-.-.-.'), tree.dict) #true - -.-.-.==> ; so it exist in morse dictionary
+        self.assertNotEqual(tree.find('-.-.-.'), 'A') #false - -.-.-. ==> ;
 
 if __name__ == '__main__':
+    print('\n\n\n----------------------------- UNIT TESTING ---------------------------\n')
+    tree = MorseTree()
     unittest.main()
